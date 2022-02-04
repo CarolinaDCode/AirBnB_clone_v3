7c0e0893dfc6787e982bad28e54a35d2dd1ffd1b
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-04 13:54:49 -0500

    status json

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+import app_views from api.v1.views
+create a route /status on the object app_views that returns a JSON: status: OK:
+"""
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ return a JSON file with Status: OK """
+    return jsonify({"status": "OK"})
