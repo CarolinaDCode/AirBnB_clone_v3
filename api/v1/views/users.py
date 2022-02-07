66972465426dd7ef152ddda7cc7e8456b51d78ee
CarolinaDCode <dianacarhuamanta03@gmail.com>
2022-02-07 12:02:27 -0500

    users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -35,7 +35,7 @@ def delete_user(user_id):
         abort(404)
     storage.delete(user)
     storage.save()
-    return jsonify({}),200    
+    return jsonify({}), 200
 
 
 @app_views.route('/users', methods=['POST'],
