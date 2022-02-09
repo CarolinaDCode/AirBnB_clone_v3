1151c922e772349e3fcd14fb5a4130097caeab7b
luisdev20 <lescobedo.im@gmail.com>
2022-02-08 23:29:49 -0500

    Task 12 done

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -6,7 +6,7 @@ from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
 from models.place import Place
-from models.state import State
+from models.user import User
 from models.review import Review
 
 
